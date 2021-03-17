7aab57cdd2291983e97569910fb4cb83c87658d1
f1rehaz4rd <wcjon123@gmail.com>
2021-03-16 20:08:40 -0700

    Add querying actions

--- teamserver/pkg/rest/executedaction.go
+++ teamserver/pkg/rest/executedaction.go
@@ -1,1 +1,38 @@
 package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/ProjectHivemind/Teamserver/teamserver/pkg/crud"
+	"github.com/gorilla/mux"
+)
+
+func getExecutedActions(w http.ResponseWriter, r *http.Request) {
+	var d crud.DatabaseModel
+	d.Open()
+	defer d.Close()
+
+	actions, err := d.AllExecutedActions()
+	if err != nil {
+		fmt.Fprint(w, GENERAL_ERROR)
+	} else {
+		json.NewEncoder(w).Encode(actions)
+	}
+}
+
+func getExecutedAction(w http.ResponseWriter, r *http.Request) {
+	var d crud.DatabaseModel
+	d.Open()
+	defer d.Close()
+
+	id := mux.Vars(r)["id"]
+	action, err := d.GetExecutedActionById(id)
+
+	if err != nil {
+		fmt.Fprint(w, GENERAL_ERROR)
+	} else {
+		json.NewEncoder(w).Encode(action)
+	}
+}
--- teamserver/pkg/rest/serve.go
+++ teamserver/pkg/rest/serve.go
@@ -30,8 +30,16 @@ func Start(port string) {
 	// Module Funcs
 
 	// Stored Action Funcs
+	router.Path("/storedaction").HandlerFunc(getStoredActions).Methods("GET")
+	router.Path("/storedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getStoredAction).Methods("GET")
+	router.Path("/storedaction").HandlerFunc(createStoredAction).Methods("POST")
+	router.Path("/storedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteStoredAction).Methods("DELETE")
 
 	// Staged Action Funcs
+	router.Path("/stagedaction").HandlerFunc(getStagedActions).Methods("GET")
+	router.Path("/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getStagedAction).Methods("GET")
+	router.Path("/stagedaction").HandlerFunc(createStagedAction).Methods("POST")
+	router.Path("/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteStagedAction).Methods("DELETE")
 
 	// Executed Action Funcs
 
