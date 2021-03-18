471651dedd8b81664e91ad1829c7c5f59fe6b43b
f1rehaz4rd <wcjon123@gmail.com>
2021-03-18 16:37:29 -0700

    Add /api to all of the API endpoints

--- teamserver/pkg/rest/serve.go
+++ teamserver/pkg/rest/serve.go
@@ -14,39 +14,39 @@ func Start(port string) {
 	router := mux.NewRouter()
 
 	// Implant Functions
-	router.Path("/implant").HandlerFunc(getImplants).Methods("GET")
-	router.Path("/implant/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getImplant).Methods("GET")
-	router.Path("/implanttype").HandlerFunc(getImplantTypes).Methods("GET")
-	router.Path("/implanttype/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getImplantType).Methods("GET")
-	router.Path("/callback/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getCallBack).Methods("GET")
+	router.Path("/api/implant").HandlerFunc(getImplants).Methods("GET")
+	router.Path("/api/implant/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getImplant).Methods("GET")
+	router.Path("/api/implanttype").HandlerFunc(getImplantTypes).Methods("GET")
+	router.Path("/api/implanttype/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getImplantType).Methods("GET")
+	router.Path("/api/callback/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getCallBack).Methods("GET")
 
 	// Group Funcs
-	router.Path("/group").HandlerFunc(getGroups).Methods("GET")
-	router.Path("/group/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getGroup).Methods("GET")
-	router.Path("/group").HandlerFunc(createGroup).Methods("POST")
-	router.Path("/group/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteGroup).Methods("DELETE")
-	router.Path("/group/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(updateGroup).Methods("PUT")
+	router.Path("/api/group").HandlerFunc(getGroups).Methods("GET")
+	router.Path("/api/group/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getGroup).Methods("GET")
+	router.Path("/api/group").HandlerFunc(createGroup).Methods("POST")
+	router.Path("/api/group/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteGroup).Methods("DELETE")
+	router.Path("/api/group/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(updateGroup).Methods("PUT")
 
 	// Module Funcs
-	router.Path("/module").HandlerFunc(getModules).Methods("GET")
-	router.Path("/module/{id}").HandlerFunc(getModule).Methods("GET")
-	router.Path("/modulefunc/{id}").HandlerFunc(getModuleFuncs).Methods("GET")
+	router.Path("/api/module").HandlerFunc(getModules).Methods("GET")
+	router.Path("/api/module/{id}").HandlerFunc(getModule).Methods("GET")
+	router.Path("/api/modulefunc/{id}").HandlerFunc(getModuleFuncs).Methods("GET")
 
 	// Stored Action Funcs
-	router.Path("/storedaction").HandlerFunc(getStoredActions).Methods("GET")
-	router.Path("/storedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getStoredAction).Methods("GET")
-	router.Path("/storedaction").HandlerFunc(createStoredAction).Methods("POST")
-	router.Path("/storedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteStoredAction).Methods("DELETE")
+	router.Path("/api/storedaction").HandlerFunc(getStoredActions).Methods("GET")
+	router.Path("/api/storedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getStoredAction).Methods("GET")
+	router.Path("/api/storedaction").HandlerFunc(createStoredAction).Methods("POST")
+	router.Path("/api/storedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteStoredAction).Methods("DELETE")
 
 	// Staged Action Funcs
-	router.Path("/stagedaction").HandlerFunc(getStagedActions).Methods("GET")
-	router.Path("/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getStagedAction).Methods("GET")
-	router.Path("/stagedaction").HandlerFunc(createStagedAction).Methods("POST")
-	router.Path("/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteStagedAction).Methods("DELETE")
+	router.Path("/api/stagedaction").HandlerFunc(getStagedActions).Methods("GET")
+	router.Path("/api/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getStagedAction).Methods("GET")
+	router.Path("/api/stagedaction").HandlerFunc(createStagedAction).Methods("POST")
+	router.Path("/api/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(deleteStagedAction).Methods("DELETE")
 
 	// Executed Action Funcs
-	router.Path("/stagedaction").HandlerFunc(getExecutedActions).Methods("GET")
-	router.Path("/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getExecutedAction).Methods("GET")
+	router.Path("/api/stagedaction").HandlerFunc(getExecutedActions).Methods("GET")
+	router.Path("/api/stagedaction/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}").HandlerFunc(getExecutedAction).Methods("GET")
 
 	fmt.Printf("RESTAPI on 0.0.0.0:%s", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
