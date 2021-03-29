075de0357f28b40bd84682f727c36e02f079c149
f1rehaz4rd <wcjon123@gmail.com>
2021-03-29 10:57:38 -0700

    Add YAML config that starts different listeners

--- teamserver/cmd/hivemind/server.go
+++ teamserver/cmd/hivemind/server.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"github.com/ProjectHivemind/Teamserver/teamserver/pkg/conf"
 	"github.com/ProjectHivemind/Teamserver/teamserver/pkg/listeners/tcp"
 	"github.com/ProjectHivemind/Teamserver/teamserver/pkg/rest"
 )
 
 func main() {
-	go tcp.StartListener("1234")
-	rest.Start("4321")
+	var configOptions conf.ConfOptions
+	configOptions.GetConf("../../config/config.yaml")
+
+	// Start the different listeners
+	for _, val := range configOptions.Listeners {
+		for k, v := range val {
+			switch k {
+			case "tcp":
+				if v["enabled"] == "true" {
+					go tcp.StartListener(v["port"])
+				}
+			default:
+				break
+			}
+		}
+	}
+
+	rest.Start(configOptions.Restapi["port"])
 }
